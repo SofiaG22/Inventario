96aa238ee9af16dfadfde77c7b36c14945b47984
Sofia <sofiagarciafranco20\@gmail.com>
2023-11-15 23:24:13 -0500

    PrimerProyecto

---
+++ proveedor.php
@@ -0,0 +1,24 @@
+<form action="" method="post">
+<input type="submit" name="closeSession" value="Cerrar sesión"></input>
+</form>
+<?php
+include('validateSession/validate.php');
+include('template/navbar.php');
+?>
+<h1>Añadir proveedor</h1>
+<h2>Ver proveedor</h2>
+<form action="">
+    <label for="">Nombre-Proveedor</label>
+    <input type="text" name="nameProvider">
+    <label for="">Documento</label>
+    <input type="number" name="documentProvider">
+    <label for="">Telefono</label>
+    <input type="number" name="numberProvider">
+    <label for="">Producto</label>
+    <input type="text" name="product">
+    <label for="">Cantidad</label>
+    <input type="number" name="quantityProduct">
+    <label for="">Precio-Compra</label>
+    <input type="number" name="priceBuyProduct">
+    <input type="submit">
+</form>
--- public/productos.php
+++ public/productos.php
@@ -1,5 +1,4 @@
 <form action="" method="post">
-
 <input type="submit" name="closeSession" value="Cerrar sesión"></input>
 </form>
 <?php
@@ -8,4 +7,14 @@ include('template/navbar.php');
 ?>
 <h1>Añadir productos</h1>
 <h2>Ver productos</h2>
-<form action=""></form>
+<form action="">
+    <label for="">Nombre</label>
+    <input type="text" name="nameProduct">
+    <label for="">Codigo</label>
+    <input type="number" name="numberProduct">
+    <label for="">Descripcion</label>
+    <input type="text" name="descripcionProduct">
+    <label for="">Precio</label>
+    <input type="number" name="priceProduct">
+    <input type="submit">
+</form>
