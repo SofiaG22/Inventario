d594f3dd4e7513d0c2e0a081273566c06ef9cbf2
wilmerx5 <wilmercampos2004@gmail.com>
2023-11-17 23:56:52 -0500

    editarP

--- public/handlers/producthandler.php
+++ public/handlers/producthandler.php
@@ -1,6 +1,6 @@
 <?php
-include('clases/producto.php');
 include('conexion/conexion.php');
+include('clases/producto.php');
 if( isset($_POST['submitProduct'])){
     if( !empty($_POST['numberProduct']) && !empty($_POST['nameProduct'])  && !empty($_POST['descripcionProduct'])&& !empty($_POST['priceProduct']) && !empty($_POST['quantityProduct']) ){
         $producto =new Producto($_POST['numberProduct'],$_POST['nameProduct'],$_POST['descripcionProduct'],$_POST['priceProduct'],$_POST['quantityProduct'],$_SESSION['store']);
@@ -12,4 +12,13 @@ if( isset($_POST['submitProduct'])){
 if(isset($_POST['showProducts'])){
  Producto::getProducts($conex, $_SESSION['store']);
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['showProducts']) && !isset($_POST['submitProduct'])) {
+    foreach ($_POST as $campo => $valor) {
+        // Recorre todos los elementos en el array $_POST
+        $id=$campo;
+    }
+    Producto::updateProduct($conex,$_SESSION['store'],$id);
+}
+
 ?>
