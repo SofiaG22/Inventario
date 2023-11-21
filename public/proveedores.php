353ec06c05b96f1811beae92e51a5135986d48ff
wilmerx5 <wilmercampos2004@gmail.com>
2023-11-20 23:45:54 -0500

    actualizacion productos

--- public/proveedores.php
+++ public/proveedores.php
@@ -1,5 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php
 include('validateSession/validateTemplate.php');
 include('template/navbar.php');
@@ -7,7 +7,7 @@ include('template/navbar.php');
 
 <div class="container mt-5">
     <h1 class="text-center">Añadir proveedor</h1>
-    <form action="" class="col-md-6 offset-md-3">
+    <form action="" class="col-md-6 offset-md-3" method="post">
         <div class="mb-3">
             <label for="nameProvider" class="form-label">Nombre-Proveedor</label>
             <input type="text" class="form-control" id="nameProvider" name="nameProvider">
@@ -34,5 +34,7 @@ include('template/navbar.php');
     <input type="submit"  value="Ver proveedores" name ="showProveedor">
 </form>
 
-
+<?php
+include("handlers/providerHandler.php");
+?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
