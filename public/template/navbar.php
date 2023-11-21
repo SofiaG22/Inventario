254129173b8c9ff0c0375897e34b318bd8ea03c0
Sofia <sofiagarciafranco20\@gmail.com>
2023-11-20 23:49:01 -0500

    PrimerProyecto

--- public/template/navbar.php
+++ public/template/navbar.php
@@ -12,6 +12,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 
     <a class="navbar-brand" href="#">Tu Empresa</a>
+    <i class="fa fa-book"></i>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -44,5 +45,5 @@
 </nav>
 
 
-
+<script src="https://kit.fontawesome.com/8eee5f58eb.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- public/template/tiendaForm.php
+++ public/template/tiendaForm.php
@@ -6,41 +6,28 @@ include("../validateSession/validatenTemplate.php")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Document</title>
 </head>
 <body>
-    <h1>Crear tienda</h1>
-    <div class="container">
-        <div class="row"></div>
-            <div class="col"></div>
-                <form action="" method="post" class="row">
-
-                    <div class="col-6">
-                        <label class="col-2 col-form-label" for="">Nombre Tienda</label>
-                            <div class="col-10">
-                                <input type="text"  id="nameStore" name="nameStore">
-                            </div>
-                    </div>
-
-                    <div class="col-6">
-                        <label class="col-2 col-form-label" for="">Dirección Tienda</label>
-                            <div>
-                                <input type="text" id="adressStore"name="adressStore">
-                            </div>
-                    </div>
-
-                    <input type="submit" name="submitStore">
-            
-                    <?php
-                        include("../handlers/addStore.php")
-                    ?>
-                </form>
+    <h1 class="text-center">Crear tienda</h1>
+        <form action="" method="post" class="col-md-6 offset-md-3 mt-4">
+            <div class="mb-3">
+                <label for="createStore" class="form-label">Nombre de la tienda</label>
+                <input type="text" class="form-control" id="createStore" name="createStore">
             </div>
-        </div>
-    </div>
+            <div class="mb-3">
+                <label for="storeAddress" class="form-label">Direccion de la tienda</label>
+                <input type="password" class="form-control" id="storeAddress" name="storeAddress">
+            </div>
 
+            <button type="submit" class="btn btn-primary mx-auto d-block" name="submitLogin" onclick="(e)=>{e.preventDefault()}">Iniciar Sesión</button>
+    <?php
+    include("../handlers/addStore.php");
+    ?>
+</form>
+    
     <a href="formSignUp.php">Añadir admin</a>
    <a href="../index.php">Iniciar sesion</a>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
