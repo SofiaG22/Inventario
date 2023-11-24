aea438b535b541a939e6a0ce805dbbc90ce462ce
wilmerx5 <wilmercampos2004@gmail.com>
2023-11-23 22:54:44 -0500

    asdsa

--- public/template/indexAuth.php
+++ public/template/indexAuth.php
@@ -14,19 +14,17 @@ $_SESSION['filaBought']=10;
 include('navbar.php');
 include('handlers/indexAuth.php');
 ?>
-<form action="" method="post">
-<div class="daySell">
+<form action="" method="post" class="formSells">
+<div class="daySell divSells">
     <button type="submit" name="showSellsToday">Ver ventas del dia</button>
 </div>
-<div class="totalsellAdmin">
+<div class="totalsellAdmin divSells">
     <button type="submit" name="showSellsAdmin">Ver total vendido admin</button>
 </div>
-<div class="totalSellDay">
+<div class="totalSellDay divSells">
     <button type="submit" name="getTotalSell" >Ver total vendido tienda</button>
 </div>
-<?php
-//include('handlers/indexAuthHandler.php')
-?>
+
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
